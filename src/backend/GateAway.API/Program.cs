0cb2d36bb364c0d47b2c26fa693d22ea98ea6924
Muqimjon <muqimjon.mr@gmail.com>
2023-10-13 19:39:45 +0500

    Created ocelot configuration and startup file has been modified to support microservices

--- src/backend/GateAway.API/Program.cs
+++ src/backend/GateAway.API/Program.cs
@@ -1,8 +1,26 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// 1 ---------------------- Add file configurations --------------------------------------------/
+// ---------------------------------------------------------------------------  For Gate Away  /
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)////////----------------/
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);//////----------------/
+//------------------------------------------------------------------------------------------/
+
+
+// Add services to the container.
 builder.Services.AddControllers();
+
+
+// 2 --------------- Add ----------------------------/
+// --------------------------------  For Gate Away  /
+builder.Services.AddOcelot();//////----------------/
+//------------------------------------------------/
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,5 +39,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+// 3 -- Integration with web app --------------/
+// --------------------------  For Gate Away  /
+app.UseOcelot().Wait();//////----------------/
+//------------------------------------------/
+
 
 app.Run();
